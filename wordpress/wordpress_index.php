4e5cfab3c31320526322b330a3acb1228fbfa05e
lucky <307479836@qq.com>
2013-01-24 17:05:36 +0800

    Create luckygit in laptop

---
+++ wordpress/wordpress_index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require('./wp-blog-header.php');
+?>
+<script type="text/javascript">var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F6d6fcfafb38f64721fbeb6e9280f8752' type='text/javascript'%3E%3C/script%3E"));</script>
