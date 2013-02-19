5a0dc40cd863ac1566867a7d2767d07949204bf2
lucky <307479836@qq.com>
2013-02-19 13:30:11 +0800

    update-2.19

---
+++ wordpress/index.php
@@ -0,0 +1,27 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title>Welcome To Lucky's</title>
+<script type="text/javascript">
+img1=new Image()
+img1.src="/frontpage/1.gif"
+img2=new Image()
+img2.src="/frontpage/2.gif"
+img3=new Image()
+img3.src="/frontpage/3.gif"
+function show1(){document.b1.src =img1.src}
+function show2(){document.b1.src =img2.src}
+function show3(){document.b1.src =img3.src}</script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="icon" href="./frontpage/icon1.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="/frontpage/icon2.ico" type="image/x-icon" /><style type="text/css">
+body { background-color: #e9e9e9; font-family: "Courier New", "Courier"; font-size: 12px; color: #999999;}
+a {font-family: "Courier New", "Courier"; font-size: 15px; color: #777777; text-decoration:none}
+a:hover {color: #EEEEEE;}
+div.main {margin-top:20px; text-align: center;}
+</style></head><body><div class="main"><table border="0" cellpadding="0" cellspacing="0"  align="center">
+<br><br><br><tr><img src="./frontpage/1.gif" width="673" height="450" hiborder="0" alt="Lucky's!" name="b1" onmouseover="show2()" onmouseout="show1()" onmousedown="show3()" onmouseup="show2()"/></tr>
+<br><br><tr><td align="center"><a href="http://www.lucky521.com/wordpress">进入我的博客</a></td></table></div>
+</body></html>
+<script type="text/javascript">var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-37277660-1']); _gaq.push(['_trackPageview']);
+(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>
+
+<?php //wp_cumulus_insert(); ?>
