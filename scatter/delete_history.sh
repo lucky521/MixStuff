b5ab76bc025a685473efd86a320f82df22172f5a
lucky <307479836@qq.com>
2013-12-20 09:26:27 +0800

    update

--- scatter/delete_history.sh
+++
@@ -1,8 +0,0 @@
-rm -rf .git/refs/original/
-git reflog expire --expire=now --all
-git gc --prune=now
-git gc --aggressive --prune=now
-
-git filter-branch --index-filter 'git rm --cached --ignore-unmatch ./code2012/refresh_nkbt.py' --prune-empty --tag-name-filter cat -- --all
-
-git push origin master --force
